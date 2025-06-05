219baf42810ecdc0feb0a24810f1c23759f33cd2
Benjy <123623302+brrock@users.noreply.github.com>
2025-06-05 07:29:50 +0100

    feat: openAI compatitable models - start

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,16 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
+import { ifParsed, data as openAIProviderData } from "./.openAILike";
+import logger from "lib/logger";
+import { IS_VERCEL_ENV } from "lib/const";
+
+if (!ifParsed && !IS_VERCEL_ENV) {
+  logger.error(
+    "Error: openAILike data not parsed and weare not on Vercel. Exiting. Try refactoring the config or remaking it.",
+  );
+  process.exit(1);
+}
+
 export default () => {
   const nextConfig: NextConfig = {
     cleanDistDir: true,
@@ -10,6 +21,9 @@ export default () => {
       NO_HTTPS: process.env.NO_HTTPS,
       NEXT_PUBLIC_GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
       NEXT_PUBLIC_GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+      ...(ifParsed && !IS_VERCEL_ENV
+        ? { OPENAI_LIKE_DATA: openAIProviderData }
+        : {}),
     },
   };
   const withNextIntl = createNextIntlPlugin();
