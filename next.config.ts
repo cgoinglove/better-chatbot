16510ff619e1bad152e94e45a9ffdcfd2f9df17b
Benjy <123623302+brrock@users.noreply.github.com>
2025-06-09 07:49:27 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-import logger from "lib/logger";
 
 export default () => {
   const nextConfig: NextConfig = {
