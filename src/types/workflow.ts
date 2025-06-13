ad95e519c4a46dc13ac7edf5ef3e57124c88435c
cgoing <neo.cgoing@gmail.com>
2025-06-13 13:25:52 +0900

    enhance workflow types with new node definitions

--- src/types/workflow.ts
+++ src/types/workflow.ts
@@ -1,3 +1,6 @@
+import { UIMessage } from "ai";
+import { ChatModel } from "./chat";
+
 export type NodeType =
   | "start"
   | "end"
@@ -5,7 +8,8 @@ export type NodeType =
   | "llm"
   | "tool"
   | "code"
-  | "http";
+  | "http"
+  | "information";
 
 export type NodeMetadata = {
   position: {
@@ -15,19 +19,43 @@ export type NodeMetadata = {
   [key: string]: any;
 };
 
-export type BaseNode = {
+export type Edge = {
+  id: string;
+  source: string;
+  target: string;
+  sourceHandle?: string | null;
+  targetHandle?: string | null;
+};
+
+export type BaseNode<Type extends NodeType> = {
+  type: Type;
   id: string;
   name: string;
   description: string;
   metadata: NodeMetadata;
+  isMergeNode?: boolean;
+  usageFields: string[];
+  generateFields: string[];
 };
 
-export type StartNode = BaseNode & {
-  type: "start";
-  input: any;
+export type StartNode = BaseNode<"start"> & {
+  inputSchema: "text"; // todo: json schema
 };
 
-export type EndNode = BaseNode & {
-  type: "end";
+export type EndNode = BaseNode<"end"> & {
   output: any;
+};
+
+export type LLMNode = BaseNode<"llm"> & {
+  model: ChatModel;
+  messages: {
+    role: "user" | "assistant" | "system";
+    parts: Extract<UIMessage["parts"][number], { type: "text" }>[];
+  };
+  outputSchema: "text"; // todo: json schema
+};
+
+export type InformationNode = BaseNode<"information"> & {
+  title: string;
+  description?: string;
 };
