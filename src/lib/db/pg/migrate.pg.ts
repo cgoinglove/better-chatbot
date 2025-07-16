0553b555d96731a0b7be222a35b3873f01f9f8e9
choi sung keun <86150470+cgoinglove@users.noreply.github.com>
2025-07-17 00:04:19 +0900

    Update environment configuration and improve Docker setup

--- src/lib/db/pg/migrate.pg.ts
+++ src/lib/db/pg/migrate.pg.ts
@@ -8,6 +8,12 @@ export const runMigrate = async () => {
   const start = Date.now();
   await migrate(pgDb, {
     migrationsFolder: join(process.cwd(), "src/lib/db/migrations/pg"),
+  }).catch((err) => {
+    console.error(
+      `❌ PostgreSQL migrations failed. check the postgres instance is running.`,
+      err.cause,
+    );
+    throw err;
   });
   const end = Date.now();
 
