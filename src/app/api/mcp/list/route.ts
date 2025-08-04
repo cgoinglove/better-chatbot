45e869ec7899df4c26efc265dcae2bae4b4ca10e
cgoing <neo.cgoing@gmail.com>
2025-08-04 15:48:41 +0900

    Revert "asd"
    This reverts commit 2410dfd25da3f4a020828c154f9dab241f57d5b0.

--- src/app/api/mcp/list/route.ts
+++ src/app/api/mcp/list/route.ts
@@ -1,16 +1,7 @@
 import { mcpClientsManager } from "lib/ai/mcp/mcp-manager";
-import { BASE_URL } from "lib/const";
 
 export async function GET() {
   const list = await mcpClientsManager.getClients();
-  console.log({
-    BASE_URL,
-    vercel: process.env.VERCEL_URL,
-    port: process.env.PORT,
-    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
-  });
-
-  console.dir(process.env);
   const result = list.map(({ client, id }) => {
     return {
       ...client.getInfo(),
