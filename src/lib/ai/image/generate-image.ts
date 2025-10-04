f7d6474e90cb4f36dab8e8f5c7f044fad22c550d
cgoing <neo.cgoing@gmail.com>
2025-10-05 00:19:11 +0900

    feat: Google GenAI를 사용하여 이미지 생성 기능 추가 및 referenceImages 옵션 추가

--- src/lib/ai/image/generate-image.ts
+++ src/lib/ai/image/generate-image.ts
@@ -8,8 +8,18 @@ import { GeneratedFile, experimental_generateImage, generateText } from "ai";
 
 type GenerateImageOptions = {
   prompt: string;
-  n?: number;
+  referenceImages?: { mimeType: string; url?: string; base64?: string }[];
   abortSignal?: AbortSignal;
+};
+
+type GeneratedImage = {
+  content: UploadContent;
+  mimeType?: string;
+};
+
+type GeneratedImageResult = {
+  images: GeneratedImage[];
+  text?: string;
 };
 
 export async function generateImageWithOpenAI(
@@ -19,7 +29,6 @@ export async function generateImageWithOpenAI(
     model: openai.image("gpt-image-1"),
     abortSignal: options.abortSignal,
     prompt: options.prompt,
-    n: options.n,
   }).then((res) => res.images);
 }
 
@@ -30,7 +39,6 @@ export async function generateImageWithXAI(
     model: xai.image("grok-2-image"),
     abortSignal: options.abortSignal,
     prompt: options.prompt,
-    n: options.n,
   }).then((res) => res.images);
 }
 
@@ -44,3 +52,94 @@ export async function generateImageWithGoogle(
   });
   return result.files;
 }
+
+import { GoogleGenAI, Part as GeminiPart } from "@google/genai";
+import { safe } from "ts-safe";
+import { UploadContent } from "lib/file-storage/file-storage.interface";
+
+async function getBase64Data(image: {
+  mimeType: string;
+  url?: string;
+  base64?: string;
+}): Promise<{ base64: string; mimeType: string }> {
+  if (image.base64) {
+    return {
+      base64: image.base64,
+      mimeType: image.mimeType,
+    };
+  }
+  if (image.url) {
+    const response = await fetch(image.url);
+    const buffer = await response.arrayBuffer();
+    return {
+      base64: Buffer.from(buffer).toString("base64"),
+      mimeType: image.mimeType,
+    };
+  }
+  throw new Error("No image data provided");
+}
+
+export const generateImageWithNanoBanana = async (
+  options: GenerateImageOptions,
+): Promise<GeneratedImageResult> => {
+  const ai = new GoogleGenAI({
+    apiKey: process.env.GOOGLE_GENERATIVE_AI_API_KEY,
+  });
+  const prompt = options.prompt;
+
+  const referenceImages: GeminiPart[] = await safe(
+    options.referenceImages || [],
+  )
+    .map(async (images) =>
+      Promise.all(
+        images.map(async (image) => await getBase64Data(image)) as GeminiPart[],
+      ),
+    )
+    .unwrap();
+
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash-image",
+    contents: [
+      {
+        role: "system",
+        parts: [
+          {
+            text: `You are a helpful assistant that generates images based on a prompt.  
+You must always generate images that are relevant to the prompt and context.  
+If the prompt is unclear, you should still generate an image without asking the user for more information.`,
+          },
+        ],
+      },
+      {
+        role: "user",
+        parts: [
+          {
+            text: prompt,
+          },
+          ...referenceImages,
+        ],
+      },
+    ],
+  });
+
+  return (
+    response.candidates?.reduce(
+      (acc, candidate) => {
+        acc.text += candidate.content?.parts
+          ?.filter((part) => part.text)
+          .map((p) => p.text);
+
+        const images =
+          candidate.content?.parts
+            ?.filter((part) => part.inlineData)
+            .map((p) => ({
+              content: Buffer.from(p.inlineData!.data!, "base64"),
+              mimeType: p.inlineData!.mimeType,
+            })) ?? [];
+        acc.images.push(...images);
+        return acc;
+      },
+      { images: [] as GeneratedImage[], text: "" },
+    ) || { images: [] as GeneratedImage[], text: "Empty Images" }
+  );
+};
