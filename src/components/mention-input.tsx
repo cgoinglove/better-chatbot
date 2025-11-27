64216390ded6df135918d93275168685ef36d00a
Roman <theuargb@gmail.com>
2025-11-27 20:46:58 +0000

    fix: scope blur to mobile-only

--- src/components/mention-input.tsx
+++ src/components/mention-input.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useIsMobile } from "@/hooks/use-mobile";
 import Mention from "@tiptap/extension-mention";
 import {
   EditorContent,
@@ -70,6 +71,7 @@ export default function MentionInput({
   onBlur,
   fullWidthSuggestion = false,
 }: MentionInputProps) {
+  const isMobile = useIsMobile();
   const [open, setOpen] = useState(false);
   const position = useRef<{
     top: number;
@@ -212,7 +214,9 @@ export default function MentionInput({
       if (isSubmit) {
         e.preventDefault();
         onEnter?.();
-        editor?.commands.blur();
+        if (isMobile) {
+          editor?.commands.blur();
+        }
       }
     },
     [editor, onEnter, open],
